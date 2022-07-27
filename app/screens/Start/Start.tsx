06e88bdf0ee8c0a62db85660ad1265c21a5bcb8f
Simon <radoszszymon@gmail.com>
2022-07-27 20:07:55 +0200

    start screen - prepared modal swipe list container

--- app/screens/Start/Start.tsx
+++ app/screens/Start/Start.tsx
@@ -1,5 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import {SafeAreaView, Dimensions, StyleSheet, View} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {
+    SafeAreaView,
+    Dimensions,
+    StyleSheet,
+    View,
+    Text,
+    Button,
+} from 'react-native';
 import MainScreenHeader from './MainScreenHeader/MainScreenHeader';
 import MapView from 'react-native-maps';
 import SelectCategory from './SelectCategory/SelectCategory';
@@ -10,12 +17,56 @@ import ButtonComponent from './../../components/Utils/ButtonComponent';
 import {useSelector, useDispatch} from 'react-redux';
 import lang from './../../lang/Start/Start';
 import {setDateFrom, setDateTo} from './../../store/searchFilter/actions';
+import {Modalize} from 'react-native-modalize';
 
 interface MainScreenProps {
     navigation: any;
 }
 
 const Start = ({navigation}: MainScreenProps) => {
+    const modalizeRef = useRef<Modalize>(null);
+
+    const onOpen = () => {
+        modalizeRef.current?.open();
+    };
+
+    const getData = () => {
+        return [
+            {
+                id: 5,
+                title: 'Test #1',
+                description: 'Test',
+                category_id: 1,
+                date: '2022-07-27',
+                members_limit: 1,
+                author_id: 1,
+                latitude: 51.509865,
+                longitude: -0.118092,
+                created_at: '2022-07-27T17:41:10.000000Z',
+                updated_at: '2022-07-27T17:41:10.000000Z',
+            },
+            {
+                id: 6,
+                title: 'Test #2',
+                description: 'Test 2 desc',
+                category_id: 6,
+                date: '2022-07-29',
+                members_limit: 2,
+                author_id: 1,
+                latitude: 51.528354567701,
+                longitude: -0.12985293202157,
+                created_at: '2022-07-27T17:44:09.000000Z',
+                updated_at: '2022-07-27T17:44:09.000000Z',
+            },
+        ];
+    };
+
+    const renderItem = item => (
+        <View>
+            <Text>{item.title}</Text>
+        </View>
+    );
+
     const dispatch = useDispatch();
 
     const userToken = useSelector((state: any) => state?.User?.details?.token);
@@ -60,6 +111,27 @@ const Start = ({navigation}: MainScreenProps) => {
         setSelectCategory(false);
     };
 
+    const handleClose = dest => {
+        if (modalizeRef.current) {
+            modalizeRef.current.close(dest);
+        }
+    };
+
+    const renderContent = () => (
+        <View style={styles.content}>
+            <Text style={styles.content__subheading}>
+                {'Introduction'.toUpperCase()}
+            </Text>
+            <Text style={styles.content__heading}>Always open modal!</Text>
+            <Text style={styles.content__description}>
+                jsjdsjds dsdsjkdskmd dskmdskkdms dkskmdkmds kmdsmkdskm sdm
+                kmdskm kmdsmkds kdsms kdsmkds
+            </Text>
+            {/* <Button name="Close to initial position" onPress={() => handleClose('alwaysOpen')} />
+          <Button name="Close completely" onPress={handleClose} /> */}
+        </View>
+    );
+
     return (
         <>
             {showSelectCategory ? (
@@ -93,6 +165,16 @@ const Start = ({navigation}: MainScreenProps) => {
                         selectedCategoryName={activeCategory?.name}
                         selectedDateRange={`${lang.from[activeLanguage]}: ${activeDateFrom}\n${lang.to[activeLanguage]}: ${activeDateTo}`}
                     />
+
+                    <Modalize
+                        ref={modalizeRef}
+                        modalStyle={styles.content__modal}
+                        alwaysOpen={200}
+                        handlePosition="inside"
+                        modalTopOffset={150}>
+                        {renderContent()}
+                    </Modalize>
+
                     <MapView
                         initialRegion={
                             activeLanguage === 'pl'
@@ -129,12 +211,8 @@ const Start = ({navigation}: MainScreenProps) => {
                             />
                         ) : null}
                     </View>
-
-                    <BottomPanel
-                        data-test="BottomPanel"
-                        navigation={navigation}
-                    />
                 </View>
+                <BottomPanel data-test="BottomPanel" navigation={navigation} />
             </SafeAreaView>
         </>
     );
@@ -165,5 +243,42 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'center',
         width: Dimensions.get('screen').width,
+    },
+
+    content: {
+        padding: 20,
+    },
+
+    content__modal: {
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 6},
+        shadowOpacity: 0.45,
+        shadowRadius: 16,
+
+        flex: 0,
+    },
+
+    content__subheading: {
+        marginBottom: 2,
+
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#ccc',
+    },
+
+    content__heading: {
+        fontSize: 24,
+        fontWeight: '600',
+        color: '#333',
+    },
+
+    content__description: {
+        paddingTop: 10,
+        paddingBottom: 10,
+
+        fontSize: 15,
+        fontWeight: '200',
+        lineHeight: 22,
+        color: '#666',
     },
 });
